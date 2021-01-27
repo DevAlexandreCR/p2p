762ca02607e1783dee5f2892f808da60dcaacbc5
AlexandreCR <devalexandrecr@gmail.com>
2021-01-27 17:02:04 -0500

    feat: add order by filter component

--- resources/js/components/OrderByComponent.vue
+++ resources/js/components/OrderByComponent.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="row ms-2">
+  <div class="row">
     <div class="col-6">
-      <select class="form-select form-select-sm" aria-label="select order" name="orderBy" @change="submit()">
-        <option selected :value="'created_at'">Order By</option>
-        <option v-for="by in orderBy" :value="by" :key="by">{{ by }}</option>
+      <select class="form-select form-select-sm" aria-label="select order" name="orderBy" v-model="orderBy">
+        <option selected :value="orderBySelected">{{ orderBySelected }}</option>
+        <option v-for="by in fields" :value="by" :key="by">{{ by }}</option>
       </select>
     </div>
     <div class="col-6">
-      <select class="form-select form-select-sm" aria-label="select order" name="order" @change="submit()">
-        <option selected :value="'asc'">Asc</option>
+      <select class="form-select form-select-sm" aria-label="select order" name="order" @change="submit($event)">
+        <option selected :value="orderSelected">{{ orderSelected }}</option>
+        <option :value="'asc'">Asc</option>
         <option :value="'desc'">Desc</option>
       </select>
     </div>
@@ -21,10 +22,11 @@ export default {
 
   data () {
     return {
-      orderBy: [
+      fields: [
         'price',
         'name'
-      ]
+      ],
+      orderBy: this.orderBySelected
     }
   },
 
@@ -32,12 +34,32 @@ export default {
     formId: {
       type: String,
       required: true
+    },
+    orderBySelected: {
+      type: String,
+      required: false,
+      default: 'Order By'
+    },
+    orderSelected: {
+      type: String,
+      required: false,
+      default: 'Order'
     }
+
   },
 
   methods: {
-    submit: function () {
-      document.getElementById(this.formId).submit()
+    submit: function (event) {
+      let form = document.getElementById(this.formId)
+      let inputOrder = form.appendChild(document.createElement('input'))
+      inputOrder.setAttribute('name', 'order')
+      inputOrder.setAttribute('type', 'hidden')
+      inputOrder.value = event.target.value
+      let inputOrderBy = form.appendChild(document.createElement('input'))
+      inputOrderBy.setAttribute('name', 'orderBy')
+      inputOrderBy.setAttribute('type', 'hidden')
+      inputOrderBy.value = this.orderBy
+      form.submit()
     }
   }
 }
--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,20 +1,14 @@
 <div class="container">
-    <div class="row row-cols-3 pt-3 justify-content-end">
-        <div class="col-sm-4 d-block d-lg-none">
-            <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#modalFilters" aria-expanded="false" aria-controls="collapseExample">
-                {{ trans('fields.filters') }}
-            </button>
+    <div class="row pt-3">
+        <div class="col-sm-4">
+            <order-by-component
+                    :order-selected="'{{request()->get('order', 'Asc')}}'"
+                    :order-by-selected="'{{request()->get('orderBy', 'Order by')}}'"
+                    :form-id="'searchProductHome'">
+            </order-by-component>
         </div>
-        <div class="col-sm-4 d-none d-lg-block">
-            <order-by-component :form-id="'searchProductHome'"></order-by-component>
-        </div>
-        <div class="col-sm-4 d-none d-lg-inline-flex">
-            <label for="customRange1" class="form-label">min</label>
-            <input type="range" class="form-range" id="customRange1">
-            <label for="customRange2" class="form-label">max</label>
-            <input type="range" class="form-range" id="customRange2">
-        </div>
-        <div class="col-lg-4 col-8">
+        <div class="col-sm-4 py-2"></div>
+        <div class="col-sm-4">
             <form class="d-flex" action="{{ route('home') }}" method="get" id="searchProductHome">
                 <input class="form-control me-2" type="search" name="name" value="{{ request()->get('name') }}"
                        placeholder="{{ trans('actions.search') }}" aria-label="Search">
